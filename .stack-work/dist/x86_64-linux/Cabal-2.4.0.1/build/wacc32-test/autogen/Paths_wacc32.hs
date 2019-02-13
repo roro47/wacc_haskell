e2457fca3729c13b294c4dfdade4213813e29a5e
roro47 <qianyi.shu17@imperial.ac.uk>
2019-02-13 10:54:55 +0000

    by pass time limit

---
+++ .stack-work/dist/x86_64-linux/Cabal-2.4.0.1/build/wacc32-test/autogen/Paths_wacc32.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_wacc32 (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/bin"
+libdir     = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/lib/x86_64-linux-ghc-8.6.3/wacc32-0.1.0.0-FxmdZ82ouAfEEqrri5LsL8-wacc32-test"
+dynlibdir  = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/lib/x86_64-linux-ghc-8.6.3"
+datadir    = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/share/x86_64-linux-ghc-8.6.3/wacc32-0.1.0.0"
+libexecdir = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/libexec/x86_64-linux-ghc-8.6.3/wacc32-0.1.0.0"
+sysconfdir = "/home/parallels/lab/wacc_32/.stack-work/install/x86_64-linux/lts-13.7/8.6.3/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "wacc32_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "wacc32_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "wacc32_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "wacc32_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "wacc32_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "wacc32_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
