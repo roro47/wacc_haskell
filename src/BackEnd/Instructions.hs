539b1d68207681e5bd45ab4c20a058fa2adac3b4
Tianyi Zuo <tz2617@sprite24.doc.ic.ac.uk>
2019-02-15 15:57:14 +0000

    Added some of the show instances

--- src/BackEnd/Instructions.hs
+++ src/BackEnd/Instructions.hs
@@ -4,17 +4,39 @@ import Data.Int
 
 
 data REG = PC | LR | SP | R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 |
-           R11 | R12
-data OP =  IMM_OFS REG Int8 | MSG Int | R REG | IMM Int
-ata Lable = Lable String
+           R11 | R12 deriving Show
+
+data OP = MSG Int | R REG | IMM Int -- immediate values need to be restricted size
+type Lable = String
 data Suffix = S | None  -- if specified, update the flags
-data REGt = REGT REG | EMPTY -- REGtional REGerand
-data SLType = B | SB | H | SH | W -- only saw sb in ref compiler
-data SLOP2 = PRE Reg Int | POST Reg Int | Imm Reg Int -- IF int is zero dont show
+data Opt = OPT REG | EMPTY -- Optional Operand
+data SLType = B | SB | H | SH | W deriving Show -- only saw sb in ref compiler
+data SLOP2 = PRE REG Int | POST REG Int | Imm REG Int -- IF int is zero dont show
 
 
 {- Cond not included here :  HS/CS LO/CC because I don't knwo which to use-}
 data Cond = EQ | NE | MI | PL | VS | VC | HI | LS | GE | LT | GT | LE | AL
+            deriving Show
+
+showS :: Show a => a -> String  --use this for everything
+showS a
+  | a' == "" = ""
+  | otherwise = " " ++ a'
+    where
+      a' = show a
+
+instance Show OP where
+  show (IMM int) = "#" ++ show int
+  show (R reg) = show reg
+  show (MSG int) = "=msg_" ++ show int  -- did not include shift *
+
+instance Show Suffix where
+  show S = "S"
+  show _ = ""
+
+instance Show Opt where
+  show (OPT reg) = show reg
+  show _ = ""
 
 {- Instructions not included here: cpy adc abc neg mul cmn mul
    bic mvn tst bx blx ldrh ldrsh ldrb ldmia strh stmia cps setend
@@ -23,12 +45,11 @@ data Cond = EQ | NE | MI | PL | VS | VC | HI | LS | GE | LT | GT | LE | AL
 {- Instructions that are here but not in the instruction summary:
    smull (suffix) (cond) REG REG REG REG -}
 
-
 {- MOVS seems to be not in the reference compiler-}
 data Instr = ADD Suffix Cond REG REG OP | MOV Cond REG OP | SUB Suffix Cond REG REG OP |
-             CMP Cond REG OP | AND Suffix Cond REG REG Op | EOR Suffix Cond REG REG Op |
-             ORR Suffix Cond REG REG Op | LSL  Suffix Cond REG REG REG |
-             LSR Suffix Cond REG REG REG | ASR Suffix Cond REG REG Reg |
-             ROR Suffix Cond REG REG Reg | B Cond Lable | BL Cond Lable |
+             CMP Cond REG OP | AND Suffix Cond REG REG OP | EOR Suffix Cond REG REG OP |
+             ORR Suffix Cond REG REG OP | LSL  Suffix Cond REG REG REG |
+             LSR Suffix Cond REG REG REG | ASR Suffix Cond REG REG REG |
+             ROR Suffix Cond REG REG REG | Branch Cond Lable | BL Cond Lable |
              PUSH Cond [REG] | POP Cond [REG] | SMULL Suffix Cond REG REG REG REG |
              LDR SLType Cond REG SLOP2 | STR SLType Cond REG SLOP2
